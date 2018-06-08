96c018810b1023713f8a7ff4aa8bd5c14f077150
Mark <mmelville@ancestry.com>
2018-06-08 13:12:17 -0600

    allow bash argument for bucket name

--- deploySam.sh
+++ deploySam.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
-aws cloudformation package --template-file ./sam-template.yml --s3-bucket stine-lambda-staging --output-template-file packaged-sam-template.yml
+
+# override this default with -b or --s3-bucket
+S3_BUCKET=stine-lambda-staging
+
+## Parse Args ##
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -b|--s3-bucket) S3_BUCKET="$2"; shift; shift;;
+        *) shift ;;
+    esac
+done
+set -- "${POSITIONAL[@]}"
+## done parsing args
+
+aws cloudformation package --template-file ./sam-template.yml --s3-bucket "$S3_BUCKET" --output-template-file packaged-sam-template.yml
 
 aws cloudformation deploy --template-file ./packaged-sam-template.yml --stack-name my-sam-stack --capabilities CAPABILITY_IAM
